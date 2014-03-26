2c425e50d2f3debb59320d867903059ed13d1802
Miguel Angel Torres <miguelangel.torres@outlook.com>
2014-03-26 13:47:49 +0100

    Modificando el contenido home

--- index.php
+++ index.php
@@ -49,6 +49,11 @@
                     <h2><img src="img/mensajeriaurgente.gif" alt="Mensajeria Urgente"></h2>
                     <p><img src="img/2_p4.jpg" alt="Mensajeria rapida" class="image_aling_text img-rounded">En todos nuestros servicios DemoTrans, S.L. busca cumplir un objetivo, proporcionar un servicio de entrega de documentos y paquetería con la mayor rapidez y la mayor seguridad. Para ofrecer este servicio contamos con un equipo especializado y exclusivo, que además cuenta para su localización y seguimiento con la tecnología más avanzada del mercado. Por ello, sus documentos o paquetes están constantemente controlados pudiendo así dar un servicio puntual y sin ningún tipo de riesgos.  </p>
                     <p><a class="btn btn-default" href="#" role="button">Más información &raquo;</a></p>
+                    <div class="imagen_home row">
+                        <div class="col-md-4">Servicio Marítimo<img src="img/servicios_img_home/aereo.jpg" alt="serivicio aereo"></div>
+                        <div class="col-md-4">Servicio Aereo<img src="img/servicios_img_home/aereo.jpg" alt="serivicio aereo"></div>
+                        <div class="col-md-4">Servicio Terrestre<img src="img/servicios_img_home/aereo.jpg" alt="serivicio aereo"></div>
+                    </div>
                 </div>
                 <div class="col-md-4">
                     <h2>
---
+++ mensajeria_urgente_home.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+    Created on : 24-mar-2014, 20:59:50
+    Author     : Mitosu
+    Name: Miguel Angel Torres 
+ */
+
+
+
