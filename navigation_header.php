0516855d90d6675a30ef03ba8870a064c299797f
Miguel Angel Torres <miguelangel.torres@outlook.com>
2014-03-29 00:37:36 +0100

    Media query agregadas, faltan configurar. Esto se aplica a div contenedor del mapa para ajustar la vista del iframe a las diferentes resoluciones de pantallas.

---
+++ navigation_header.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+    Created on : 24-mar-2014, 20:59:50
+    Author     : Mitosu
+    Name: Miguel Angel Torres 
+ */
+
+echo '<ol class="breadcrumb">
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Library</a></li>
+  <li class="active">Data</li>
+</ol>';
+
